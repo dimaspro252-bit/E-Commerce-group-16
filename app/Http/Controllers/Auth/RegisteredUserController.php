d17b5d55c64f294423664cdb78769356d812f760
FixersRizal <mutiasheptichifamba@gmail.com>
2025-12-07 17:56:30 +0700

    revisi file migration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => 'buyer',
+            'role' => 'member',
         ]);
 
         event(new Registered($user));
